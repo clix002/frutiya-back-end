cb62a95917d4345b08de230f0ab613b375b0ade9
clix002 <clinton.dev002@gmail.com>
2025-12-02 11:20:03 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# frutiya-back-end
---
+++ src/main/java/com/frutiya/backend/frutiya_back_end/FrutiyaBackEndApplication.java
@@ -0,0 +1,23 @@
+package com.frutiya.backend.frutiya_back_end;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrutiyaBackEndApplication {
+
+	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure()
+				.directory(".")
+				.ignoreIfMissing()
+				.load();
+		
+		dotenv.entries().forEach(entry ->
+			System.setProperty(entry.getKey(), entry.getValue())
+		);
+		
+		SpringApplication.run(FrutiyaBackEndApplication.class, args);
+	}
+
+}
